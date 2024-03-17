2c71c2a53940963d7ce8705840c536f10020bf2f
Dev-JeanSantos <jeancbsan@gmail.com>
2024-03-17 14:19:37 -0300

    Implementation detekt and klint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,62 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
-	id("org.springframework.boot") version "3.2.3"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("io.gitlab.arturbosch.detekt") version "1.23.5"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "com.academy.fourtk"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
-}
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Detekt
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.5")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+
+tasks.compileKotlin { }
+
+// sourceSets{ getByName()}
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks.withType<Detekt> {
+    description = "Runs detekt static analisys."
+    setSource(files("src/main/kotlin"))
+    config.setFrom(files("config/detekt/detekt.yml"))
+    debug = true
+    autoCorrect = true
+    finalizedBy("ktlintCheck")
+    reports {
+        html.required.set(true)
+        html.outputLocation.set(file("build/reports/detekt.html"))
+    }
 }
+// tasks.withType<Test> {
+// 	useJUnitPlatform()
+// 	dependsOn("ktlintCheck, detekt")
+// }
--- src/main/kotlin/com/academy/fourtk/miles/generator/Main.kt
+++ src/main/kotlin/com/academy/fourtk/miles/generator/Main.kt
@@ -2,10 +2,13 @@ package com.academy.fourtk.miles.generator
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("com.academy.fourtk.*")
 class Application
 
+@Suppress("SpreadOperator")
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+    runApplication<Application>(*args)
 }

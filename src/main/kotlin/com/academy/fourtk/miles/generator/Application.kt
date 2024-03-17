95a72cf0079c33b1c6eef1ecddd9fa8af2351f65
Dev-JeanSantos <jeancbsan@gmail.com>
2024-03-17 11:26:08 -0300

    starting project of register miles

---
+++ src/main/kotlin/com/academy/fourtk/miles/generator/Application.kt
@@ -0,0 +1,11 @@
+package com.academy.fourtk.miles.generator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
